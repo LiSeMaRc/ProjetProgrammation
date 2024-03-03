04b8efccf0586d3a368fb10d9e23f71e810b7723
Lise <lise.marchal@ensae.fr>
2024-03-03 17:55:59 +0100

    tests bfs. bfs simple grille 2 ne tourne pas mais sûrement trop lourd

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -26,12 +26,14 @@ for i in range(1,20):
 for i in range(1,20):
     for j in range(i+1,21):
         print("Question 5, graph2",i,j,Graph.bfs(Graph.graph_from_file("input\\graph2.in"),i,j))
+"""
 
 #Question 7: bfs for grid
-print("Question 7, grid0",Grid.resolution(Grid.grid_from_file("input\\grid0.in")))
-print("Question 7, grid1",Grid.resolution(Grid.grid_from_file("input\\grid1.in")))
+#print("Question 7, grid0",Grid.resolution(Grid.grid_from_file("input\\grid0.in")))
+#print("Question 7, grid1",Grid.resolution(Grid.grid_from_file("input\\grid1.in")))
+print("Question 7, grid2",Grid.resolution(Grid.grid_from_file("input\\grid2.in")))
 
-
+"""
 #Question 8: short bfs for grid
 print("Question 8, grid0",Grid.resolution_short(Grid.grid_from_file("input\\grid0.in")))
 print("Question 8, grid1",Grid.resolution_short(Grid.grid_from_file("input\\grid1.in")))
@@ -46,8 +48,8 @@ print("Question 8, grid2",Grid.resolution_short(Grid.grid_from_file("input\\grid
 #print("Question 9, grid1",Grid.a_star(Grid.grid_from_file("input\\grid1.in")))
 
 #print("Question 8, grid2",Grid.resolution_short(Grid.grid_from_file("input\\grid2.in")))
-
-#print("Question 9, grid2",Grid.a_star(Grid.grid_from_file("input\\grid2.in")))
+#grid=Grid(6, 6, [[34, 20, 26, 1, 25, 13], [12, 35, 11, 31, 7, 27], [6, 2, 17, 36, 30, 21], [9, 29, 8, 3, 16, 18], [32, 15, 4, 22, 14, 19], [24, 33, 23,10,28,5]])
+#print("Question 9, grid2",grid.a_star())
 
 #for grid in Grid.adj_grids(Grid.grid_from_file("input\\grid2.in")):
     #print(grid.node())
